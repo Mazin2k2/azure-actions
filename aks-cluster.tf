2aa5f989ad9c2b580af0f117277d9c87ad6d94c1
Mazin2k2 <141407535+Mazin2k2@users.noreply.github.com>
2024-12-10 19:56:40 +0530

    Update aks-cluster.tf

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -1,1 +1,21 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = "vprofile-aks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+}
 
