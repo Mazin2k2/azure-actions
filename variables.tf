e0e0a437dd49fdb162eb7b40722aff1e4565b93e
Mazin2k2 <141407535+Mazin2k2@users.noreply.github.com>
2024-12-10 19:55:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
+variable "location" {
+  description = "Azure region"
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  default     = "vprofile-aks-rg"
+}
+
+variable "cluster_name" {
+  description = "AKS cluster name"
+  default     = "vprofile-aks-cluster"
+}
+
+variable "node_count" {
+  description = "Number of AKS nodes"
+  default     = 2
+}
 
