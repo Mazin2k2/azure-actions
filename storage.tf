2bdea14026f21b2e8b5a764112e550335cdd0e84
Mazin2k2 <141407535+Mazin2k2@users.noreply.github.com>
2024-12-10 19:57:12 +0530

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,1 +1,8 @@
+resource "azurerm_storage_account" "sa" {
+  name                     = "terraformstorageacc"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
