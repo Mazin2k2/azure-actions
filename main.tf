0821965422c8d5c637085f44f6e7894af69b465d
Mazin2k2 <141407535+Mazin2k2@users.noreply.github.com>
2024-12-10 19:54:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-backend-rg"
+    storage_account_name = "terraformstorageacc"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
 
