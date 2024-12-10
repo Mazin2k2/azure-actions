b281424e531bb6198ab63d09a9f05348712bd043
Mazin2k2 <141407535+Mazin2k2@users.noreply.github.com>
2024-12-10 19:56:56 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,12 @@
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "kubeconfig" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
 
